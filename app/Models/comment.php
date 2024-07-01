b99dcc7d8522bd167c714ea8f1606dbcfbf76005
MD. SAZZAD <mdsazzad0002@gmail.com>
2024-07-02 00:24:56 +0600

    d

--- app/Models/comment.php
+++ app/Models/comment.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\post;
+use App\Models\vote;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class comment extends Model
 {
@@ -20,6 +22,6 @@ class comment extends Model
     }
 
     public function post(){
-        return $this->hasOne(Post::class, 'id', 'post_id');
+        return $this->hasOne(post::class, 'id', 'post_id');
     }
 }
